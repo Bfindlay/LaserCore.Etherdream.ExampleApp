fe2b7931b03b9bad4f83131068fe081e7dd9d176
Brett Findlay <brett@dawnpatroltv.com>
2021-06-14 18:33:52 +1000

    initial commit

---
+++ EtherDream.Net.ExampleApp/Form1.Designer.cs
@@ -0,0 +1,154 @@
+﻿
+namespace EtherDream.Net.ExampleApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.DeviceName = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.DevName = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.FileName = new System.Windows.Forms.Label();
+            this.StartLaser = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(308, 126);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(183, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Load File";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.loadFile);
+            // 
+            // DeviceName
+            // 
+            this.DeviceName.AutoSize = true;
+            this.DeviceName.Location = new System.Drawing.Point(-334, -5);
+            this.DeviceName.Name = "DeviceName";
+            this.DeviceName.Size = new System.Drawing.Size(42, 15);
+            this.DeviceName.TabIndex = 1;
+            this.DeviceName.Text = "Device";
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.OrangeRed;
+            this.button2.Font = new System.Drawing.Font("Segoe UI", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.button2.Location = new System.Drawing.Point(179, 273);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(433, 165);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "STOP";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.stop);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(46, 67);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(106, 15);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Connected Device:";
+            // 
+            // DevName
+            // 
+            this.DevName.AutoSize = true;
+            this.DevName.Location = new System.Drawing.Point(159, 67);
+            this.DevName.Name = "DevName";
+            this.DevName.Size = new System.Drawing.Size(42, 15);
+            this.DevName.TabIndex = 4;
+            this.DevName.Text = "Device";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(46, 82);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(70, 15);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "File Loaded:";
+            // 
+            // FileName
+            // 
+            this.FileName.AutoSize = true;
+            this.FileName.Location = new System.Drawing.Point(159, 82);
+            this.FileName.Name = "FileName";
+            this.FileName.Size = new System.Drawing.Size(57, 15);
+            this.FileName.TabIndex = 6;
+            this.FileName.Text = "FileName";
+            // 
+            // StartLaser
+            // 
+            this.StartLaser.BackColor = System.Drawing.Color.Chartreuse;
+            this.StartLaser.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.StartLaser.Location = new System.Drawing.Point(179, 208);
+            this.StartLaser.Name = "StartLaser";
+            this.StartLaser.Size = new System.Drawing.Size(433, 40);
+            this.StartLaser.TabIndex = 7;
+            this.StartLaser.Text = "Start Laser";
+            this.StartLaser.UseVisualStyleBackColor = false;
+            this.StartLaser.Click += new System.EventHandler(this.StartLaserButton);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.StartLaser);
+            this.Controls.Add(this.FileName);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.DevName);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.DeviceName);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label DeviceName;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label DevName;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label FileName;
+        private System.Windows.Forms.Button StartLaser;
+    }
+}
+
---
+++ EtherDream.Net.ExampleApp/Form1.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Threading;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using LaserCore.Etherdream.Net.Discovery;
+using LaserCore.Etherdream.Net.Device;
+using LaserCore.Etherdream.Net.Dto;
+using laserCore.Ilda.Net;
+
+namespace EtherDream.Net.ExampleApp
+{
+    public partial class Form1 : Form
+    {
+        private Dac etherDream;
+        private string laserFilePath = string.Empty;
+        private CancellationTokenSource cancellationToken;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Create a device discovery opject
+            var discovery = new DeviceDiscovery();
+            // find the dac identity of the first available device
+            // NOTE v 1.0.0 of LaserCore.Etherdream will throw an exception if the 
+            // Dac was not already plugged in when the program was started so ensure its already on
+            var dac = discovery.FindFirstDevice();
+            // Create etherdream instance with the IP we found
+            etherDream = new Dac(dac.Ip);
+            this.DevName.Text = etherDream.IP;
+        }
+
+        private void StartLaserButton(object sender, EventArgs e)
+        {
+
+            if (string.IsNullOrEmpty(laserFilePath))
+            {
+                MessageBox.Show("No File Selected");
+                return;
+            }
+            using var fileStream = new FileStream(laserFilePath, FileMode.Open);
+
+            // Use lasercore ilda parser to parse ilda into a list of points
+            var ildaPoints = IldaImporter.ParseIlda(fileStream);
+
+            // The Ilda parser outputs just a standard point, we need to map it to 
+            // the dacpoint that the etherdream is expecting
+            var laserDacPoints = ildaPoints.Select(point => new DacPointDto
+            {
+                X = point.X,
+                Y = point.Y,
+                R = point.R,
+                G = point.G,
+                B = point.B,
+                I = point.I,
+                Control = 0,
+                U1 = 0,
+                U2 = 0
+            }).ToArray();
+
+            cancellationToken = new CancellationTokenSource();
+            // prepare the device,
+            // this is actually done in the streampoints method underthe hood so i need to revise this
+            etherDream.Prepare();
+            // run the device in a new thread to prevent GUI blocking
+            Task.Run(() => etherDream.StreamPoints(laserDacPoints, 12000), cancellationToken.Token);
+        }
+
+
+        private void loadFile(object sender, EventArgs e)
+        {
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.InitialDirectory = "c:\\";
+                openFileDialog.Filter = "ILDA Files (*.ild)|*.ild";
+                openFileDialog.FilterIndex = 2;
+                openFileDialog.RestoreDirectory = true;
+
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    //Get the path of specified file
+                    laserFilePath = openFileDialog.FileName;
+                    this.FileName.Text = laserFilePath;
+                }
+            }
+
+        }
+
+        private void stop(object sender, EventArgs e)
+        {
+            // stop the etherdream from outputting the laser
+            etherDream.Stop();
+            // cancel the background task
+            if (cancellationToken != null)
+                cancellationToken.Cancel();
+
+        }
+    }
+}
